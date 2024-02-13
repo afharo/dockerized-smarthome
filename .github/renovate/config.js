6d46c3b9eb7592d4eef25e1b3af105056a83e54f
Alejandro Fernández Haro <alejandro.haro@elastic.co>
2024-02-13 21:17:40 +0100

    Add access to ghcr.io

--- .github/renovate/config.js
+++ .github/renovate/config.js
@@ -4,9 +4,15 @@ module.exports = {
   repositories: [ "afharo/dockerized-smarthome" ],
   hostRules: [
     {
-      hostType: 'docker',
+      hostType: "docker",
       username: process.env.DOCKER_HUB_USER,
       password: process.env.DOCKER_HUB_PASSWORD,
+    },
+
+    // Explicitly declaring the GH docker registry to avoid using the default "token-based authentication" (that's failing)
+    {
+      hostType: "docker",
+      matchHost: "ghcr.io",
     },
   ],
 };
